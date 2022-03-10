c9e33b38a19dcdf64f158e53f973c3257fa09e79
UserStefDlt <UserStefDLT@gmail.com>
2022-03-10 16:32:55 -0700

    Pattern Weaver 🧵🌈

--- lab/waves/wave-functions/wave.js
+++ lab/waves/wave-functions/wave.js
@@ -42,9 +42,30 @@ for (let index = 0; index < 150; index++) {
 
 for (let index = 0; index < 150; index++) {
     let r0 = index - (150/2);
-    let r1 = (Math.sin(r0) * 10) + (r0 * .1);
+    // let r1 = (Math.sin(r0) * 10) + (r0 * .1);
+    let r1 = Math.cos((r0%60)/20 - 1.5) * 15;
     let r2 = Math.round(r1);
     console.log('.'.repeat(20+r2));
+    console.log('.'.repeat(20)+"|");
+}
+
+
+for (let index = 0; index < 100; index++) {
+    // let r1 = (Math.sin(r0) * 10) + (r0 * .1);
+    let r1 = Math.cos((index%60)/20 - 1.5) * 15;
+    let r2 = Math.round(r1);
+    console.log('.'.repeat(20+r2));
+    console.log('.'.repeat(20)+"|");
+}
+
+for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 20; j++) {
+        // let r1 = (Math.sin(r0) * 10) + (r0 * .1);
+        let r1 = Math.cos((i*j)*3).toFixed(2);
+        console.log(r1);
+        // console.log('.'.repeat(20+r2));
+        // console.log('.'.repeat(20)+"|");
+    }
 }
 
 
