15875e5e16760c62fcf3372186c763d6e54d3a9e
UserStefDlt <UserStefDLT@gmail.com>
2022-09-06 19:17:28 -0600

    *️⃣ Small details ☑ and Links 🔗🌐

---
+++ dev/re-organizing/nytelabs.js
@@ -0,0 +1,318 @@
+/*
+
+Organizing Nytelabst. 
+
+Laboratory Organization:
+
+Lab A. Labst.
+Lab B. NyteLabs.
+Lab C. checking.
+Lab D. for development.
+Lab E. 
+
+───
+
+Nytelabst is intended to be like a laboratory for working on different project ideas and experiments.
+
+Projects and experiments can be organized by different attributes:
+    - technologies used.
+    - intention.
+
+
+For example:
+    - for technologies used, we have things like canvas, svg, only css, javascript.
+    - for intention, we have things like functionality, design, games, etc.
+
+───────────────
+
+
+[Accomodations]: when something is meant to help with neurodiverity. (like accesibility but for mental instead of physical disabilities.)
+
+[Art]: making something visually appealing.
+
+[Craft]: recreating something. Usually something that exist physically.
+
+[Design]: *(for making things that look good.)
+
+[Experiments]: for learning how something works. I'll make an experiment and test how a project will work. These are experiments of what is possible, not something that should be displayed, but just for confirming functionality.
+
+[Games]: for experiments that deal with interaction and game mechanics.
+
+[Nexyt]: for experiments I'm starting but haven't decided where it should go.
+
+[Other]: for experiments that don't fit on a current category.
+
+[Special]: For specific ones, currently: [canvas], [svg].
+
+[Tools]: useful for other projects.
+
+[WDD]: for web design and development. What is more used for commercial purposes.
+
+─────────────── 
+
+[Accomodations]
+[Art]
+[Craft]
+[Design]
+[Experiments]
+[Games]
+[Nexyt]
+[Other]
+[Special]
+[Tools]
+[WDD]
+
+
+// ───────────────  ───────────────  ─────────────── //
+
+•[Accomodations]: 
+└[text]
+
+
+•[Art]
+
+
+•[Craft]
+
+
+•[Design]
+
+
+•[Experiments]
+
+
+•[Games]: 
+└[maze] (8)
+  └[simple]
+  └[slow]
+  └[vision]
+  └[ant]
+  └[alternative]
+  └[maze]
+  └[path]
+  └[isometric]
+└[xo]
+
+
+•[Nexyt]
+└[holonomy]
+
+•[Other]: 
+└[keyevents]
+└[other]
+  └[git-calendar]
+
+•[Special]:
+└[canvas] (21)
+  └[brush]
+  └[chasing]
+  └[cloud-brush-02]
+  └[cloud-brush]
+  └[cloud-particles]
+  └[experiment]
+  └[fireworks]
+  └[liquid-particles]
+  └[orbit-set]
+  └[orbit]
+  └[particle-collision-in-sectors]
+  └[particle-collision]
+  └[particle-exp]
+  └[rainbow-brush]
+  └[rainbow-cloud-brush]
+  └[re-experiment]
+  └[spell-effect-02]
+  └[spell-effect-03]
+  └[spell-effect]
+  └[touchTest]
+  └[try]
+└[svg]
+  └[backgrounds]
+  └[cards]
+  └[cloud-exp-02]
+  └[cloud-experiments]
+  └[clouds]
+  └[fe-water]
+  └[feTurbulence]
+  └[filter-effect]
+  └[forest-clouds]
+  └[magic-circle]
+  └[quid-experiments]
+  └[svg-filter-replicator]
+
+•[Tools]: 
+└[eval]
+  └[base-eval]
+  └[grid-eval]
+
+•[WDD]: 
+└[btns]
+└[layout] (7)
+  └[bar-graphs]
+  └[glowing-chip]
+  └[layout-3d]
+  └[login-styles]
+  └[neumorphic-design]
+  └[responsive-dashboard]
+  └[web-layout]
+
+
+
+
+// ───────────────  ───────────────  ─────────────── //
+
+ [✓, ✗, ⋯]
+
+[btns]
+    └[btn-styles]   [✗]
+    └[btn-types]    [✓]
+    └[nybtns]       [✗]
+    └[NyteBtns]     [⋯]
+
+[building]
+
+[canvas]
+
+[cards]
+
+[color]
+
+[designs]
+[dev]
+[dice]
+[eval]
+[exp]
+[holonomy]
+[keyevents]
+[layout]
+[maze]
+[other]
+[pattern-weaver]
+[recreate]
+[spheres]
+[stars]
+[svg]
+[text]
+[unknown]
+[waves]
+[xo]
+
+───
+
+
+
+•[Art]: 
+•[Craft]: 
+    └[building]
+        └[3d-rooms]
+        └[brick-wall]
+        └[grass]
+        └[tile-wf]
+        └[]
+        └[]
+    └[cards]
+        └[card-designs]
+        └[deck-20-saf]
+        └[deck-a04]
+        └[deck-sphere]
+        └[]
+
+└[dice]
+└[liquid]
+└[recreate]
+└[spheres]
+└[stars]
+└[waves]
+
+└[dotsphere]
+└[dome animation]
+
+
+└[waves]
+
+
+
+└[color]
+
+
+
+└[recreate]
+  └[glowing-circles]
+  └[keyboard]
+  └[kinetic-sculpture]
+
+└[spheres]
+
+└[stars]
+
+
+
+•[Design]: 
+└[pattern-weaver]
+└[designs]
+└[cards]
+└[color]
+
+└[designs] (6)
+  └[!unknown-exp]
+  └[accidental-designs]
+  └[bg-up]
+  └[myst-box]
+  └[tiles]
+  └[turbulence-filter]
+
+
+
+
+
+
+
+
+└[exp] (13)
+  └[creature-randomizer-01]
+  └[creature-randomizer]
+  └[dome-animation]
+  └[dotsphere]
+  └[flow-arrows]
+  └[grid-boxes]
+  └[hex-grid]
+  └[liquid]
+  └[randsphere]
+  └[spheres]
+  └[spheriment]
+  └[star-sphere]
+  └[window]
+
+
+
+
+
+───
+
+
+  └[!unknown-exp]
+  └[accidental-designs]
+  └[bg-up]
+  └[myst-box]
+  └[tiles]
+  └[turbulence-filter]
+
+  └[creature-randomizer-01]
+  └[creature-randomizer]
+  └[dome-animation]
+  └[dotsphere]
+  └[flow-arrows]
+  └[grid-boxes]
+  └[hex-grid]
+  └[liquid]
+  └[randsphere]
+  └[spheres]
+  └[spheriment]
+  └[star-sphere]
+  └[window]
+
+
+
+
+*/
+
+
---
+++ nyte.js
@@ -0,0 +1,724 @@
+/*
+
+Organizing Nytelabst. 
+
+Laboratory Organization:
+
+Lab A. Labst.
+Lab B. NyteLabs.
+Lab C. checking.
+Lab D. for development.
+Lab E. 
+
+───
+
+Nytelabst is intended to be like a laboratory for working on different project ideas and experiments.
+
+Projects and experiments can be organized by different attributes:
+    - technologies used.
+    - intention.
+
+
+For example:
+    - for technologies used, we have things like canvas, svg, only css, javascript.
+    - for intention, we have things like functionality, design, games, etc.
+
+───────────────
+
+
+[Accomodations]: when something is meant to help with neurodiverity. (like accesibility but for mental instead of physical disabilities.)
+
+[Art]: making something visually appealing.
+
+[Craft]: recreating something. Usually something that exist physically.
+
+[Design]: *(for making things that look good.)
+
+[Experiments]: for learning how something works. I'll make an experiment and test how a project will work. These are experiments of what is possible, not something that should be displayed, but just for confirming functionality.
+
+[Games]: for experiments that deal with interaction and game mechanics.
+
+[Nexyt]: for experiments I'm starting but haven't decided where it should go.
+
+[Other]: for experiments that don't fit on a current category.
+
+[Special]: For specific ones, currently: [canvas], [svg].
+
+[Tools]: useful for other projects.
+
+[WDD]: for web design and development. What is more used for commercial purposes.
+
+─────────────── 
+
+[Accomodations]
+[Art]
+[Craft]
+[Design]
+[Experiments]
+[Games]
+[Nexyt]
+[Other]
+[Special]
+[Tools]
+[WDD]
+
+
+// ───────────────  ───────────────  ─────────────── //
+
+•[Accomodations]: 
+└[text]
+
+
+•[Art]
+
+
+•[Craft]
+
+
+•[Design]
+
+
+•[Experiments]
+
+
+•[Games]: 
+└[maze] (8)
+  └[simple]
+  └[slow]
+  └[vision]
+  └[ant]
+  └[alternative]
+  └[maze]
+  └[path]
+  └[isometric]
+└[xo]
+
+
+•[Nexyt]
+└[holonomy]
+
+•[Other]: 
+└[keyevents]
+└[other]
+  └[git-calendar]
+
+•[Special]:
+└[canvas] (21)
+  └[brush]
+  └[chasing]
+  └[cloud-brush-02]
+  └[cloud-brush]
+  └[cloud-particles]
+  └[experiment]
+  └[fireworks]
+  └[liquid-particles]
+  └[orbit-set]
+  └[orbit]
+  └[particle-collision-in-sectors]
+  └[particle-collision]
+  └[particle-exp]
+  └[rainbow-brush]
+  └[rainbow-cloud-brush]
+  └[re-experiment]
+  └[spell-effect-02]
+  └[spell-effect-03]
+  └[spell-effect]
+  └[touchTest]
+  └[try]
+└[svg]
+  └[backgrounds]
+  └[cards]
+  └[cloud-exp-02]
+  └[cloud-experiments]
+  └[clouds]
+  └[fe-water]
+  └[feTurbulence]
+  └[filter-effect]
+  └[forest-clouds]
+  └[magic-circle]
+  └[quid-experiments]
+  └[svg-filter-replicator]
+
+•[Tools]: 
+└[eval]
+  └[base-eval]
+  └[grid-eval]
+
+•[WDD]: 
+└[btns]
+└[layout] (7)
+  └[bar-graphs]
+  └[glowing-chip]
+  └[layout-3d]
+  └[login-styles]
+  └[neumorphic-design]
+  └[responsive-dashboard]
+  └[web-layout]
+
+
+// ───────────────  ───────────────  ─────────────── //
+
+[✓, ✗, ⋯]
+
+[btns]
+    └[btn-styles]       [✗]
+    └[btn-types]        [✓] 'my basic styles for buttons.'
+    └[nybtns]           [✗]
+    └[NyteBtns]         [⋯] 'incomplete'
+
+[building]
+    └[3d-layers]        [⋯] 
+    └[3d-rooms]         [✓] 'from flat surfaces to an isometric room.'
+    └[blocks]           [✓] 'Design using blocks for displaying text, a logo, buttons, etc.'
+    └[brick-wall]       [✓] 'background patterns.'
+    └[grass]            [✗]
+    └[tile-patterns]    [✓] 'patterns in a grid display with styling in the tiles'
+    └[tile-patterns-02] [✗]
+    └[tile-wf]          [✓] 'design for terrain tiles for making paths.'
+    └[wave-blocks]      [✓] 'blocks animated by an equation based on their x and y axis.'
+    └[waving-tiles]     [✓] 'like tile patterns'
+
+[canvas]
+    └[experiment]                       [⋯] 'contains many experiments.'
+
+    └[ant-colony]                       [⋯] 'squiggle lines drawn in the map.'
+
+    └[particle-collision]               [✓] 'simulation of particle elastic collision.'
+    └[particle-collision-in-sectors]    [⋯] 'simulation of collision of particles evaluated by sectors to avoid evaluating all particles against eachother.'
+    └[orbit]                            [✓] 'simulation of particle collision with gravity.'
+    └[orbit-set]                        [✓] 'simulation of particle collision with gravity. +settings.'
+    └[cloud-particles]                  [✗] 'attempt to make particles look like clouds. it works, but it is not that good.'
+    └[liquid-particles]                 [✓] 'simulation of particle interaction. (collision and gravity). the particles display an effect like liquid.'
+    └[particle-exp]                     [⋯] 'particle following mouse can draw on canvas.'
+    └[chasing]                          [⋯] 'a particle following the mouse.'
+    
+    └[brush]                            [✓] 'rainbow brush. looks good, only with mouse.'
+    └[rainbow-brush]                    [✓] 'rainbow brush. adds the use of touch, but the design now shows darker colors.'
+    └[rainbow-cloud-brush]              [✓] 'draws rainbow clouds, but it is a filter in the canvas, and it won't save.'
+    └[cloud-brush]                      [✓] 'draws clouds, but it is a filter in the canvas, and it won't save.'
+    └[cloud-brush-02]                   [✓] 'draws clouds as a filter in the brush and can be saved.'
+    └[re-experiment]                    [⋯] 'a different particle effect when click.'
+    └[re-experiment/re-exp-4c.html]
+    └[re-experiment/re-exp-4d.html]
+    └[re-experiment/re-exp-4e.html]
+    
+    └[fireworks]                        [✓] 'makes fireworks when tapping on the screen.'
+    └[spell-effect]                     [✓] 'contains 3 effects for mouse move and click. [Q,W,E]'
+    └[spell-effect-02]                  [✓] 'contains 3 effects for mouse move and click. [Q,W,E]'
+    └[spell-effect-03]                  [✓] 'contains 4 effects for mouse move and click. [Q,W,E,R]'
+
+    └[stars]                            [⋯] 'flickering lights.'
+    └[touchTest]                        [✓] 'confirms the use of touch screen.'
+    └[try]                              [✗] 'visuals on angles as a visual aid for understanding the collision of particles.'
+
+[cards]
+    └[card-designs]         []
+    └[deck-20-saf]          []
+    └[deck-a04]             []
+    └[deck-sphere]          []
+
+[color]
+    └[color-check]          []
+    └[color-palette]        []
+    └[color-paths]          []
+    └[color-random]         []
+    └[color-sequence]       []
+    └[color-symmetry]       []
+
+[designs]
+    └[!unknown-exp]             []
+    └[accidental-designs]       []
+    └[bg-up]                    []
+    └[myst-box]                 []
+    └[tiles]                    []
+    └[turbulence-filter]        []
+
+[dev]
+    └[grid]                 []
+
+[dice]
+    └[d4]       []
+    └[d6]       []
+    └[d8]       []
+    └[d12]      []
+    └[d20]      []
+
+[eval]
+    └[base-eval]    []
+    └[grid-eval]    []
+
+[exp]
+    └[creature-randomizer]      []
+    └[creature-randomizer-01]   []
+    └[dome-animation]           []
+    └[dotsphere]                []
+    └[flow-arrows]              []
+    └[grid-boxes]               []
+    └[hex-grid]                 []
+    └[liquid]                   []
+    └[randsphere]               []
+    └[spheres]                  []
+    └[spheriment]               []
+    └[star-sphere]              []
+    └[tile-patterns]            []
+    └[window]                   []
+
+[holonomy]
+    └[holonomy-key-controllers] []
+    └[rotation-rings]           []
+
+[keyevents]
+    └[keydown]  []
+
+[layout]
+    └[bar-graphs]               []
+    └[cal]                      []
+    └[glowing-chip]             []
+    └[grid-data]                []
+    └[layout-3d]                []
+    └[login-styles]             []
+    └[neumorphic-design]        []
+    └[responsive-dashboard]     []
+    └[screen-grid]              []
+    └[toggle-settings]          []
+    └[web-layout]               []
+
+[maze]
+    └[ant-colony]       []
+    └[base]             []
+    └[delayed-building] []
+    └[diagonal-maze]    []
+    └[exp]              []
+    └[grid-vision]      []
+    └[hex-maze]         []
+    └[maze--style]      []
+    └[maze-alts]        []
+    └[maze-side]        []
+    └[simple]           []
+    └[tile-maze]        []
+
+    *[ant]
+    *[simple]
+    *[slow]
+    *[vision]
+    *[alternative]
+    *[maze]
+    *[path]
+    *[isometric]
+
+[other]
+    └[git-calendar]     []
+
+[pattern-weaver]
+    └[pattern-$$]*36
+
+[recreate]
+    └[glowing-circles]      []
+    └[keyboard]             []
+    └[kinetic-sculpture]    []
+
+
+[spheres]
+    └[dotsphere]    []
+    └[orbit]        []
+
+[stars]
+    └[canvas-stars]     []
+    └[night--sky]       []
+    └[style-stars]      []
+    └[the-sun]          []
+
+[svg]
+    └[backgrounds]              []
+    └[cards]                    []
+    └[cloud-exp-02]             []
+    └[cloud-experiments]        []
+    └[clouds]                   []
+    └[fe-water]                 []
+    └[feTurbulence]             []
+    └[filter-effect]            []
+    └[forest-clouds]            []
+    └[magic-circle]             []
+    └[quid-experiments]         []
+    └[svg-filter-replicator]    []
+
+[text]
+    └[half-bold]    []
+    └[set]          []
+    └[set2]         []
+
+[unknown]
+    └[grid]             []
+    └[input-range]      []
+    └[myLog]            []
+    └[wave-blur]        []
+
+[waves]
+    └[curves]               []
+    └[grid-waves]           []
+    └[grid-waves-01]        []
+    └[grid-waves-02]        []
+    └[grid-waves-03]        []
+    └[grid-waves-04]        []
+    └[particles]            []
+    └[wave-functions]       []
+    └[wave-map]             []
+
+[xo]
+    └[xo4]      []
+    └[xo6]      []
+
+
+// ───────────────  ───────────────  ─────────────── //
+
+───────────────
+
+
+
+•[Art]: 
+•[Craft]: 
+    └[building]
+        └[3d-rooms]
+        └[brick-wall]
+        └[grass]
+        └[tile-wf]
+        └[]
+        └[]
+    └[cards]
+        └[card-designs]
+        └[deck-20-saf]
+        └[deck-a04]
+        └[deck-sphere]
+        └[]
+
+└[dice]
+└[liquid]
+└[recreate]
+└[spheres]
+└[stars]
+└[waves]
+
+└[dotsphere]
+└[dome animation]
+
+
+└[waves]
+
+
+
+└[color]
+
+
+
+└[recreate]
+  └[glowing-circles]
+  └[keyboard]
+  └[kinetic-sculpture]
+
+└[spheres]
+
+└[stars]
+
+
+
+•[Design]: 
+└[pattern-weaver]
+└[designs]
+└[cards]
+└[color]
+
+└[designs] (6)
+  └[!unknown-exp]
+  └[accidental-designs]
+  └[bg-up]
+  └[myst-box]
+  └[tiles]
+  └[turbulence-filter]
+
+
+
+
+
+
+
+
+└[exp] (13)
+  └[creature-randomizer-01]
+  └[creature-randomizer]
+  └[dome-animation]
+  └[dotsphere]
+  └[flow-arrows]
+  └[grid-boxes]
+  └[hex-grid]
+  └[liquid]
+  └[randsphere]
+  └[spheres]
+  └[spheriment]
+  └[star-sphere]
+  └[window]
+
+
+
+
+
+───
+
+
+  └[!unknown-exp]
+  └[accidental-designs]
+  └[bg-up]
+  └[myst-box]
+  └[tiles]
+  └[turbulence-filter]
+
+  └[creature-randomizer-01]
+  └[creature-randomizer]
+  └[dome-animation]
+  └[dotsphere]
+  └[flow-arrows]
+  └[grid-boxes]
+  └[hex-grid]
+  └[liquid]
+  └[randsphere]
+  └[spheres]
+  └[spheriment]
+  └[star-sphere]
+  └[window]
+
+
+
+
+*/
+
+
+
+/*
+* [ ] [btn-styles](https://userstefdlt.github.io/NyteLabs/lab/btns/btn-styles/)
+* [ ] [btn-styles](lab/btns/btn-styles/)
+// ───────────────  ───────────────  ─────────────── //
+
+[btns]
+    └[btn-styles]
+    └[btn-types]
+    └[nybtns]
+    └[NyteBtns]
+
+[building]
+    └[3d-layers]
+    └[3d-rooms]
+    └[blocks]
+    └[brick-wall]
+    └[grass]
+    └[tile-patterns]
+    └[tile-patterns-02]
+    └[tile-wf]
+    └[wave-blocks]
+    └[waving-tiles]
+
+[canvas]
+    └[experiment]
+    └[ant-colony]
+    └[particle-collision]
+    └[particle-collision-in-sectors]
+    └[orbit]
+    └[orbit-set]
+    └[cloud-particles]
+    └[liquid-particles]
+    └[particle-exp]
+    └[chasing]
+    └[brush]
+    └[rainbow-brush]
+    └[rainbow-cloud-brush]
+    └[cloud-brush]
+    └[cloud-brush-02]
+    └[re-experiment]
+    └[fireworks]
+    └[spell-effect]
+    └[spell-effect-02]
+    └[spell-effect-03]
+    └[stars]
+    └[touchTest]
+    └[try]
+
+[cards]
+    └[card-designs]
+    └[deck-20-saf]
+    └[deck-a04]
+    └[deck-sphere]
+
+[color]
+    └[color-check]
+    └[color-palette]
+    └[color-paths]
+    └[color-random]
+    └[color-sequence]
+    └[color-symmetry]
+
+[designs]
+    └[!unknown-exp]
+    └[accidental-designs]
+    └[bg-up]
+    └[myst-box]
+    └[tiles]
+    └[turbulence-filter]
+
+[dev]
+    └[grid]
+
+[dice]
+    └[d4]
+    └[d6]
+    └[d8]
+    └[d12]
+    └[d20]
+
+[eval]
+    └[base-eval]
+    └[grid-eval]
+
+[exp]
+    └[creature-randomizer]
+    └[creature-randomizer-01]
+    └[dome-animation]
+    └[dotsphere]
+    └[flow-arrows]
+    └[grid-boxes]
+    └[hex-grid]
+    └[liquid]
+    └[randsphere]
+    └[spheres]
+    └[spheriment]
+    └[star-sphere]
+    └[tile-patterns]
+    └[window]
+
+[holonomy]
+    └[holonomy-key-controllers]
+    └[rotation-rings]
+
+[keyevents]
+    └[keydown]
+
+[layout]
+    └[bar-graphs]
+    └[cal]
+    └[glowing-chip]
+    └[grid-data]
+    └[layout-3d]
+    └[login-styles]
+    └[neumorphic-design]
+    └[responsive-dashboard]
+    └[screen-grid]
+    └[toggle-settings]
+    └[web-layout]
+
+[maze]
+    └[/maze/ant-colony/index.html]
+    └[/maze/base/index.html]
+    └[/maze/delayed-building/index.html]
+    └[/maze/diagonal-maze/index.html]
+    └[/maze/exp] [s]
+    └[/maze/exp/base.html]
+    └[/maze/exp/exp.html]
+    └[/maze/exp/exp-nds-$$.html]@2*12
+    └[/maze/exp/index-2.html]
+    └[/maze/grid-vision/index.html]
+    └[/maze/hex-maze] [x]
+    └[/maze/maze--style/index.html]
+    └[/maze/maze-alts/index.html]
+    └[/maze/maze-side] [s]
+    └[/maze/maze-side/exp.html] 
+    └[/maze/maze-side/index.html] 
+    └[/maze/simple] [s]
+    └[/maze/simple/ant-maze-nolog.html]
+    └[/maze/simple/maze-3d.html]
+    └[/maze/simple/maze-paths.html]
+    └[/maze/simple/maze-side.html]
+    └[/maze/simple/maze-vision.html]
+    └[/maze/simple/maze-zoom.html]
+    └[/maze/simple/maze.html]
+    └[/maze/simple/simple.html]
+    └[/maze/simple/slow.html]
+    └[tile-maze] [x]
+
+    *[ant]
+    *[simple]
+    *[slow]
+    *[vision]
+    *[alternative]
+    *[maze]
+    *[path]
+    *[isometric]
+
+[other]
+    └[/other/git-calendar/index.html]
+
+[pattern-weaver]
+    └[most-recent]
+    └[pattern-$$]*36
+
+[recreate]
+    └[/recreate/glowing-circles/index.html]
+    └[/recreate/keyboard/index.html]
+    └[/recreate/kinetic-sculpture/index.html]
+
+
+[spheres]
+    └[/spheres/dotsphere/index.html]
+    └[/spheres/orbit/index.html]
+
+[stars]
+    └[/stars/canvas-stars/stars-index.html]
+    └[/stars/night--sky/index.html]
+    └[style-stars] [x]
+    └[/stars/the-sun/index.html]
+
+[svg]
+    └[/svg/backgrounds/index.html]
+    └[/svg/cards/index.html]
+    └[/svg/cloud-exp-02/index.html]
+    └[/svg/cloud-experiments/index.html]
+    └[/svg/clouds/index.html]
+    └[/svg/fe-water/index.html]
+    └[/svg/feTurbulence/index.html]
+    └[/svg/filter-effect/index.html]
+    └[/svg/forest-clouds/index.html]
+    └[/svg/magic-circle/index.html]
+    └[/svg/quid-experiments/index.html]
+    └[svg-filter-replicator] [x]
+
+[text]
+    └[/text/half-bold/index.html]
+    └[/text/set/index.html]
+    └[/text/set2/index.html]
+
+[unknown]
+    └[/unknown/grid/index.html]
+    └[/unknown/input-range/index.html]
+    └[/unknown/myLog/index.html]
+    └[/unknown/wave-blur/index.html]
+
+[waves]
+    └[/waves/curves/index.html]
+    └[/waves/grid-waves/index.html]
+    └[/waves/grid-waves-01/index.html]
+    └[/waves/grid-waves-02/index.html]
+    └[/waves/grid-waves-03/index.html]
+    └[/waves/grid-waves-04/index.html]
+    └[/waves/particles/index.html]
+    └[/waves/wave-functions/index.html]
+    └[/waves/wave-map/index.html]
+
+[xo]
+    └[/xo/xo4/index.html]
+    └[/xo/xo6/index.html]
+
+// ───────────────  ───────────────  ─────────────── //
+
+*/
+
+/*
+to get links to all the html files...
+
+
+*/
